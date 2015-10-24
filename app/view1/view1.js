90ca2d1509093d9788f3a7c28824208e6a1d9adb
Herpiko Dwi Aguno <herpiko@gmail.com>
2015-10-24 15:37:43 +0700

    Chart for dht11

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,54 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ["$scope", "$rootScope", "$http", function($scope, $rootScope,  $http) {
+  $rootScope.socket = io("http://128.199.95.141:2999");
+  
+  $rootScope.socket.on("whoareu", function(){
+    console.log("middleware asking whoareu");
+    $rootScope.socket.emit("join-ng");
+  })
+  $scope.dht11Data = [
+    [1],
+    [2]
+  ]
+  $scope.dht11Series = ["Temperature", "Humidity"];
+
+  var updateDht11 = function(data){
+    if ($scope.dht11Data[0].length >= 10) {
+     $scope.dht11Data[0].shift();
+    } 
+    if ($scope.dht11Data[1].length >= 10) {
+     $scope.dht11Data[1].shift();
+    } 
+    $scope.dht11Data[0].push(data.temp);
+    $scope.dht11Data[1].push(data.humid);
+    console.log($scope.dht11Data);
+  $scope.$apply();
+  }
+
+  $rootScope.socket.on("message", function(data){
+    if (data.sensor == "dht11") {
+      updateDht11(data.data); 
+    }
+  })
+  $scope.labels = ["9","8","7","6","5","4","3","2","1","0"];
+  $scope.series = ['Temperature', 'Humidity'];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+
+  $scope.led1 = function(value){
+    $http({
+      method : "POST",
+      url : "http://128.199.95.141:2999/api/led1?value=" + value
+    })
+    .success(function(data, status){
+      console.log(data);
+    })
+    .error(function(data, status){
+      console.log(data);
+    })
+  }
 
 }]);
